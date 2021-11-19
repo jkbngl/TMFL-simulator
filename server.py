32db75b3815e1bdc77177a39597f21da2eeb4420
jkbngl <jakob.engl@hotmail.de>
2021-11-19 20:48:40 +0100

    removed default types

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 from datetime import datetime, timedelta, date
 import numpy as np
 import matplotlib.pyplot as plt
